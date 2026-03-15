69f3bda935201a159081961acf42abe642550ec3
Cyanhana_Neko <111105781+RunrunXi@users.noreply.github.com>
2026-03-15 20:56:30 +0800

    feat:添加回响工具充能相关内容

--- src/main/java/me/cyanhana/amethyst_tech/ModEvents.java
+++ src/main/java/me/cyanhana/amethyst_tech/ModEvents.java
@@ -3,6 +3,7 @@ package me.cyanhana.amethyst_tech;
 import me.cyanhana.amethyst_tech.helper.IntrinsicEnchantmentHelper;
 import me.cyanhana.amethyst_tech.item.EchoItem;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.ExperienceOrb;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
@@ -18,27 +19,41 @@ public class ModEvents {
     @SubscribeEvent
     public static void getXp(PlayerXpEvent.PickupXp e) {
         if (e.getEntity() instanceof ServerPlayer serverPlayer) {
-            int orbValue = e.getOrb().getValue();
-            repairPlayerItems(serverPlayer, orbValue);
+            ExperienceOrb orb = e.getOrb();
+            int orbValue = orb.getValue();
+            int i = repairAndChargeItems(serverPlayer, orbValue);
+            if (i <= 0) {
+                e.setCanceled(true);
+                orb.discard();
+            }
         }
-
     }
-    private static void repairPlayerItems(ServerPlayer player, int value) {
-        Optional<ItemStack> optional = IntrinsicEnchantmentHelper.getRandomItemStackWith(player, ItemStack::isDamaged);
+    // 修复耐久
+    private static int repairAndChargeItems(ServerPlayer player, int value) {
+        // 获取当前装备的回响工具
+        Optional<ItemStack> optional = IntrinsicEnchantmentHelper.getRandomItemStackWith(
+                player,
+                stack -> stack.getItem() instanceof EchoItem
+        );
         if (optional.isPresent()) {
             ItemStack itemstack = optional.get();
-            Item item = itemstack.getItem();
-//            if (item instanceof IntrinsicEnchantItem intrinsicEnchantItem) {
-//                intrinsicEnchantItem.getIntrinsicEnchantLevel(itemstack, Enchantments.MENDING);
-//            }
-            if (item instanceof EchoItem)
-            {
+            EchoItem echoItem = (EchoItem) itemstack.getItem();
+            // 修复耐久
+            if (itemstack.isDamaged()) {
                 int i = EnchantmentHelper.modifyDurabilityToRepairFromXp(player.serverLevel(), itemstack, (int) (value * itemstack.getXpRepairRatio()));
                 int j = Math.min(i, itemstack.getDamageValue());
                 itemstack.setDamageValue(itemstack.getDamageValue() - j);
             }
+            // 充能工具
+            int currentXpCharge = echoItem.getXpCharge(itemstack);
+            int newCharge = Math.min(currentXpCharge + value, EchoItem.MAX_CHARGE);
+            echoItem.setXpCharge(itemstack, newCharge);
 
+            int chargeGained = newCharge - currentXpCharge;
+            return value - chargeGained;
         }
+        return value;
     }
+
 
 }
--- src/main/java/me/cyanhana/amethyst_tech/item/EchoItem.java
+++ src/main/java/me/cyanhana/amethyst_tech/item/EchoItem.java
@@ -1,4 +1,14 @@
 package me.cyanhana.amethyst_tech.item;
 
+import me.cyanhana.amethyst_tech.ModDataComponents;
+import net.minecraft.world.item.ItemStack;
+
 public interface EchoItem {
+    int MAX_CHARGE = 500;
+    default int getXpCharge(ItemStack stack) {
+        return stack.getOrDefault(ModDataComponents.XP_CHARGE, 0);
+    }
+    default void setXpCharge(ItemStack stack, int value) {
+        stack.set(ModDataComponents.XP_CHARGE, Math.min(value, MAX_CHARGE));
+    }
 }
